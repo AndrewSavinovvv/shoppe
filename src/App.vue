72ff11b5bd77ea6d0dfd77315d16e5fe4c060ea8
Eternal Hunger <eternalhunger@MacBook-Air-Eternal.local>
2024-11-04 19:53:41 +0200

    test range

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
 import HomeView from "@/views/HomeView.vue";
 import Qwe from "@/components/TestComponent.vue";
+import TestComponent from "@/components/TestComponent.vue";
 </script>
 
 <template>
   <HomeView></HomeView>
-  <Qwe></Qwe>
+  <TestComponent></TestComponent>
 </template>
 
 <style scoped>
